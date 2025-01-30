c623d106ac3058a09f09dbc8d343f7d27c59f776
madhavi <madhavinaidu424@gmail.com>
2025-01-30 11:19:25 +0530

    format

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const heading = React.createElement("h1",
-    {id:"heading",XYZ:"ABC"},
+    { id: "heading", XYZ: "ABC" },
     "Hello From REACT HOHOOOOOOOOOOOOOOO!!!!!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
