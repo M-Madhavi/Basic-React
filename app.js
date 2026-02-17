6158ed140f3f0fc840453e0d78b2755a88419558
Madhavi <madhavinaidu424@gmail.com>
2026-02-17 23:34:57 +0530

    create nested React Element

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+//create a nested element using React.createElement() method and render it to the DOM using ReactDOM.createRoot() method.
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "h2",
+      { id: "heading2" },
+      "Nested Element using React.createElement()",
+    ),
+    React.createElement(
+      "h2",
+      { id: "heading2" },
+      "second Nested Element using React.createElement()",
+    ),
+  ]),
+);
+
+console.log(parent);
+const nested = ReactDOM.createRoot(document.getElementById("root"));
+nested.render(parent);
+
 const heading = React.createElement(
   "h1",
   { id: "heading", XYZ: "ABC" },
@@ -5,4 +28,4 @@ const heading = React.createElement(
 );
 console.log(heading); // React.createElement() is used to create a React element. It takes three arguments: the type of element, the props, and the children. In this case, we are creating an h1 element with an id of "heading" and a custom attribute XYZ with a value of "ABC". The content of the h1 element is "Hello From REACT!!!". The console.log() statement will output the React element object to the console.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
