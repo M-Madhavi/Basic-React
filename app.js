4eab68cb5f49a68ffa886ff0e26650de45fef59c
Madhavi <madhavinaidu424@gmail.com>
2026-02-17 23:13:57 +0530

    link css

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const heading = React.createElement("h1",
-    { id: "heading", XYZ: "ABC" },
-    "Hello From REACT!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading", XYZ: "ABC" },
+  "Hello From REACT!!!",
+);
+console.log(heading); // React.createElement() is used to create a React element. It takes three arguments: the type of element, the props, and the children. In this case, we are creating an h1 element with an id of "heading" and a custom attribute XYZ with a value of "ABC". The content of the h1 element is "Hello From REACT!!!". The console.log() statement will output the React element object to the console.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
